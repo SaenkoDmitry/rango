43ac500e743ed621eb6ec03c21fafc3f2fb25742
SaenkoDmitry <dmitryssaenko@gmail.com>
2017-02-18 20:05:41 +0300

    add script for filling database and editted admin panel

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):  # Используйте __unicode в Python 2 и __str__ в Python 3
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):      # Используйте __unicode в Python 2 и __str__ в Python 3
+        return self.title
